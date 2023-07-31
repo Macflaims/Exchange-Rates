4648c57e45833be3caa47507617cd2679f022f50
Macflaims <martinsanchez2533@gmail.com>
2023-07-31 09:13:37 -0600

    add table functionality

---
+++ src/modules/table.js
@@ -0,0 +1,40 @@
+import {getDate} from "../main.js";
+
+export function setCurrencyTable(base, date="latest"){
+    fetch(`https://api.exchangerate.host/${date}?base=${base}`)
+    .then(response => response.json())
+    .then(responseJSON=>{
+        document.querySelector("#currency-table").textContent=""
+        Object.entries(responseJSON.rates).forEach(currency=>{
+        const $newCurrency = document.createElement("th");
+        $newCurrency.textContent = currency[0];
+        
+        const $newConversion = document.createElement("td");
+        $newConversion.textContent = currency[1];
+
+        const $newRow = document.createElement("tr");
+        $newRow.appendChild($newCurrency);
+        $newRow.appendChild($newConversion)
+
+        document.querySelector("#currency-table").appendChild($newRow);
+    })
+})
+}
+
+function removePreviousTable(){
+    document.querySelectorAll("#currency-table tr").forEach((element)=>{
+        element.remove()})
+}
+
+export function setTableCall(){
+    const $baseSelector = document.querySelector("#convert-from");
+    $baseSelector.addEventListener("change", ()=>{
+        removePreviousTable();
+        showLoading(document.querySelector("#currency-table"));
+        setCurrencyTable($baseSelector.value, getDate())
+    })
+}
+
+export function showLoading(element){
+    element.textContent="Cargando..."
+}
